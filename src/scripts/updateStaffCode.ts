3b6df4774d3287b1d3a756f0d2188609ab780a3d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-21 13:05:02 -0800

    thought

--- src/scripts/updateStaffCode.ts
+++ src/scripts/updateStaffCode.ts
@@ -1,2 +1,3 @@
 // TODO: this should be moved into being a proper script
 //  Then use the newly added force-deploy script flag to avoid failing when not matching the remote forum staffCode.js
+//  Also I think it throws an error if you don't update the forum after every single time you recompile it
