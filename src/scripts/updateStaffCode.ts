149fb3fcdf2be082bd82cee419a643ffef7ca261
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-28 17:52:18 -0800

    eliminate ScriptFlag

--- src/scripts/updateStaffCode.ts
+++ src/scripts/updateStaffCode.ts
@@ -1,3 +1,8 @@
 // TODO: this should be moved into being a proper script
 //  Then use the newly added force-deploy script flag to avoid failing when not matching the remote forum staffCode.js
 //  Also I think it throws an error if you don't update the forum after every single time you recompile it
+
+import {program} from "@sagittal/general"
+
+program
+    .option(`--force`, "force update, even if remote doesn't agree with existing dist")
