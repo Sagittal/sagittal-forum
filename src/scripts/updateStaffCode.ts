9411091846b20110f8564577af7dff82180aac5c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-02 16:40:50 -0800

    no more checking and forcing

--- src/scripts/updateStaffCode.ts
+++ src/scripts/updateStaffCode.ts
@@ -1,22 +1,18 @@
-import {program} from "@sagittal/general"
+import cp from "child_process"
 import {Client} from "ssh2"
 // @ts-ignore
 import secrets from "../../.secrets.json"
 import {ssh2} from "../globals"
 import {updateStaffCode} from "../updateStaffCode"
 
-program
-    .option(`--force`, "force update, even if remote doesn't agree with existing dist")
-    .parse()
-
-const forceUpdate = !!program.force
-
 const SSH_OPT = {...secrets, port: 26, host: "dkeenan.com"}
+
+cp.execSync("sh bin/update_staff_code.sh")
 
 const conn: Client = new Client()
 ssh2.conn = conn
 
-conn.on("ready", (): void => updateStaffCode(forceUpdate))
+conn.on("ready", updateStaffCode)
 conn.on("error", (err: Error): void => {
     throw err
 })
--- src/updateStaffCode.ts
+++ src/updateStaffCode.ts
@@ -1,35 +1,21 @@
 import {Maybe, saveLog} from "@sagittal/general"
-import cp from "child_process"
 import * as fs from "fs"
 import {SFTPWrapper} from "ssh2"
-import {compareFiles} from "./compare"
-import {LOCAL_FILE, REMOTE_FILE, TMP_FILE, TMP_VERSION_FILE} from "./constants"
+import {LOCAL_FILE, REMOTE_FILE, TMP_VERSION_FILE} from "./constants"
 import {ssh2} from "./globals"
 
-const updateStaffCode = (forceUpdate: boolean): void => {
+const updateStaffCode = (): void => {
     ssh2.conn.sftp((err: Maybe<Error>, sftp: SFTPWrapper): void => {
         if (err) throw err
 
-        sftp.fastGet(
+        sftp.fastPut(
+            LOCAL_FILE,
             REMOTE_FILE,
-            TMP_FILE,
             (err: Maybe<Error>): void => {
                 if (err) throw err
+                ssh2.conn.end()
 
-                compareFiles(forceUpdate)
-
-                cp.execSync("sh bin/update_staff_code.sh")
-
-                sftp.fastPut(
-                    LOCAL_FILE,
-                    REMOTE_FILE,
-                    (err: Maybe<Error>): void => {
-                        if (err) throw err
-                        ssh2.conn.end()
-
-                        saveLog(`\n\nThe Sagittal Forum has had its staffCode.js updated to version ${fs.readFileSync(TMP_VERSION_FILE)}.\n\n`)
-                    },
-                )
+                saveLog(`\n\nThe Sagittal Forum has had its staffCode.js updated to version ${fs.readFileSync(TMP_VERSION_FILE)}.\n\n`)
             },
         )
     })
